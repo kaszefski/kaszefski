c3811bf1f1640a730628d1db5e0a534c61c8fd5b
kaszefski <90379700+kaszefski@users.noreply.github.com>
2022-03-15 09:24:49 +0100

    Update experiment.py

--- experiment.py
+++ experiment.py
@@ -79,7 +79,7 @@ try:
     shutil.rmtree('newsjam')
 except FileNotFoundError:
     print('No previous newsjam repo was found. Cloning.')
-#subprocess.run(['git', 'clone', 'https://github.com/pie3636/newsjam.git'])
+subprocess.run(['git', 'clone', 'https://github.com/pie3636/newsjam.git'])
 os.chdir('newsjam')
 print('Installing Python modules...')
 #subprocess.run(['python3', '-m', 'pip', 'install', '-r', 'requirements.txt'])
